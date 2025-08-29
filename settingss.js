59b138ce37f4c6cfa6e8baeafdac01c8bc4a1b0c
barianbariankenku149-wq <barianbariankenku149@gmail.com>
2025-08-29 20:01:50 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~12dUzBZJ#pzpLhFcP5DzRMqSXdx4leVwo3ZKRZJ4c7yJVFcXF3yM",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "237651442921", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "🤌🏾", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
